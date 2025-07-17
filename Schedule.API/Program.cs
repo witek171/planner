74d1b92802a57a82fda4d143e8cb53647e4bfd8a
Witold <witold778899@gmail.com>
2025-07-17 14:37:03 +0200

    add health check

--- Schedule.API/Program.cs
+++ Schedule.API/Program.cs
@@ -1,6 +1,9 @@
-
 using Microsoft.Data.SqlClient;
 using System.Data;
+using Common.Application.Services;
+using Common.Application.Utils;
+using Common.Infrastructure.Services;
+using Common.Infrastructure.Utils;
 
 namespace PlannerNet
 {
@@ -9,13 +12,17 @@ namespace PlannerNet
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            
             // Add services to the container.
 
             builder.Services.AddScoped<IDbConnection>(sp =>
-            new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+                new SqlConnection(connectionString));
 
             builder.Services.AddControllers();
+            builder.Services.AddScoped<IHealthCheckService, HealthCheckService>();
+            builder.Services.AddScoped<IHealthCheckUtils, HealthCheckUtils>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -25,6 +32,7 @@ namespace PlannerNet
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }

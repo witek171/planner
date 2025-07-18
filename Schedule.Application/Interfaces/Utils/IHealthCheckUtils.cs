319fbe016d0a175721848211d0a42e9e0359fa7b
Witold <witold778899@gmail.com>
2025-07-18 15:52:38 +0200

    add interface

---
+++ Schedule.Application/Interfaces/Utils/IHealthCheckUtils.cs
@@ -0,0 +1,8 @@
+﻿namespace Schedule.Application.Interfaces.Utils;
+
+public interface IHealthCheckUtils
+{
+	string GetConnectionString();
+	string GetAssemblyVersion();
+	string MaskConnectionString(string connectionString);
+}

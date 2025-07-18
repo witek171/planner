e97da99ddd125e4c07fab3b0b88637f254ca35ce
Witold <witold778899@gmail.com>
2025-07-18 23:43:19 +0200

    add automapper profile

---
+++ Schedule.API/Mappings/MappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Schedule.Domain.Dtos;
+using Schedule.Domain.Models;
+
+namespace PlannerNet.Mappings;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<ApplicationHealthStatus, ApplicationHealthStatusDto>()
+			.ConstructUsing(src => new ApplicationHealthStatusDto(
+					src.Version,
+					src.Environment,
+					src.Uptime,
+					src.MemoryUsage,
+					src.Status,
+					src.Timestamp,
+					src.Details ?? new Dictionary<string, object>()
+				)
+			);
+
+		CreateMap<DatabaseHealthStatus, DatabaseHealthStatusDto>()
+			.ConstructUsing(src => new DatabaseHealthStatusDto(
+					src.ConnectionString,
+					src.ResponseTime,
+					src.DatabaseName,
+					src.Status,
+					src.Timestamp,
+					src.Details ?? new Dictionary<string, object>()
+				)
+			);
+	}
+}

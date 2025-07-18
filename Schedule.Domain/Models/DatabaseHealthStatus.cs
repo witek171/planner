b2a8e9b9d83c667e4889c4488d70eadff4db3953
Witold <witold778899@gmail.com>
2025-07-18 15:50:47 +0200

    add constructor

---
+++ Schedule.Domain/Models/DatabaseHealthStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace Schedule.Domain.Models;
+
+public class DatabaseHealthStatus : HealthStatusBase
+{
+	public DatabaseHealthStatus(
+		string connectionString,
+		TimeSpan responseTime,
+		string databaseName,
+		string status,
+		DateTime timestamp,
+		Dictionary<string, object> details
+	) : base(status, timestamp, details)
+	{
+		ConnectionString = connectionString;
+		ResponseTime = responseTime;
+		DatabaseName = databaseName;
+	}
+
+	public string ConnectionString { get; }
+	public TimeSpan ResponseTime { get; }
+	public string DatabaseName { get; }
+}

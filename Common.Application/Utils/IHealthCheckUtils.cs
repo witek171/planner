64ff65d22a1d940ff743e135e9f71294c341b124
Witold <witold778899@gmail.com>
2025-07-18 15:54:13 +0200

    clean up

--- Common.Application/Utils/IHealthCheckUtils.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Common.Application.Utils;
-
-public interface IHealthCheckUtils
-{
-    string GetConnectionString();
-    string GetAssemblyVersion();
-    string MaskConnectionString(string connectionString);
-}

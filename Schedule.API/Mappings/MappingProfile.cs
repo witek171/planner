f1e4105daa29f1d0c28d69b092e4af45f55a3510
Witold Pacholik <witoldpacholik@wp.pl>
2025-07-23 23:17:40 +0000

    Merged in health-checks (pull request #9)
    Health checks
    
    * add interface
    
    * add HealthCheckUtils.cs
    
    * refactor
    
    * clean up
    
    * add automapper profile
    
    * add automapper
    
    * refactor
    
    * convert dto classes to records
    
    * remove HealthStatusDto.cs
    
    * clean up
    
    * improve health check reliability and exception handling
    
    * add Degraded state handling with proper HTTP status codes
    
    * replace indentation spaces with tabs
    
    * move dtos to Schedule.Contracts
    
    * change application health check to return Unhealthy on critical errors
    
    * remove nullable from health DTO
    
    * improve error handling in GetApplicationStatus details
    
    * separate DTOs into request/response models
    
    * change 'degraded' http code and refactor
    
    * add new methods
    
    * improve exception handling
    
    * refactor
    
    * add needed PackageReference
    
    * change var to explicit type
    
    * clean up
    
    
    Approved-by: Marcin D

---
+++ Schedule.API/Mappings/MappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Schedule.Contracts.Dtos;
+using Schedule.Domain.Models;
+
+namespace PlannerNet.Mappings;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<ApplicationHealthStatus, ApplicationHealthStatusResponse>()
+			.ConstructUsing(src => new ApplicationHealthStatusResponse(
+					src.Version,
+					src.Environment,
+					src.Uptime,
+					src.MemoryUsage,
+					src.Status,
+					src.Timestamp,
+					src.Details
+				)
+			);
+
+		CreateMap<DatabaseHealthStatus, DatabaseHealthStatusResponse>()
+			.ConstructUsing(src => new DatabaseHealthStatusResponse(
+					src.ConnectionString,
+					src.ResponseTime,
+					src.DatabaseName,
+					src.Status,
+					src.Timestamp,
+					src.Details
+				)
+			);
+	}
+}
---
+++ Schedule.Contracts/Dtos/DatabaseHealthStatusResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schedule.Contracts.Dtos;
+
+public class DatabaseHealthStatusResponse
+{
+	[Required] public string ConnectionString { get; set; }
+	[Required] public TimeSpan ResponseTime { get; set; }
+	[Required] public string DatabaseName { get; set; }
+	[Required] public string Status { get; set; }
+	[Required] public DateTime Timestamp { get; set; }
+	[Required] public Dictionary<string, object> Detail { get; set; }
+
+	public DatabaseHealthStatusResponse(
+		string connectionString,
+		TimeSpan responseTime,
+		string databaseName,
+		string status,
+		DateTime timestamp,
+		Dictionary<string, object> detail
+	)
+	{
+		ConnectionString = connectionString;
+		ResponseTime = responseTime;
+		DatabaseName = databaseName;
+		Status = status;
+		Timestamp = timestamp;
+		Detail = detail;
+	}
+}

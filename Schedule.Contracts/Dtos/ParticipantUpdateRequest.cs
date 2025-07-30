b40fc26e7c4eb01757bcc71351a4ebdeafeb07af
Witold <witold778899@gmail.com>
2025-07-31 01:09:54 +0200

    add ParticipantCreateRequest.cs

---
+++ Schedule.Contracts/Dtos/ParticipantUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schedule.Contracts.Dtos;
+
+public class ParticipantUpdateRequest
+{
+	[EmailAddress] public string? Email { get; set; }
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	[Phone] public string? Phone { get; set; }
+	public bool? GdprConsent { get; set; }
+}

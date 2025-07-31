4d875f08d5e45724e8594e2d97574fd004e831e6
Witold <witold778899@gmail.com>
2025-07-31 14:05:58 +0200

    remove GDPRconsent field

--- Schedule.Contracts/Dtos/ParticipantUpdateRequest.cs
+++ Schedule.Contracts/Dtos/ParticipantUpdateRequest.cs
@@ -5,8 +5,7 @@ namespace Schedule.Contracts.Dtos;
 public class ParticipantUpdateRequest
 {
 	[EmailAddress] public string? Email { get; set; }
-	public string? FirstName { get; set; }
-	public string? LastName { get; set; }
+	[StringLength(40)] public string? FirstName { get; set; }
+	[StringLength(40)] public string? LastName { get; set; }
 	[Phone] public string? Phone { get; set; }
-	public bool? GdprConsent { get; set; }
 }

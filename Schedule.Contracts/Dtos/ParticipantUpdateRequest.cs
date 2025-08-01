bfb2524db8dc8475ab473f31ad7a9697462667f0
Witold <witold778899@gmail.com>
2025-08-01 14:00:09 +0200

    add data adnotations and make fields non nullable

--- Schedule.Contracts/Dtos/ParticipantUpdateRequest.cs
+++ Schedule.Contracts/Dtos/ParticipantUpdateRequest.cs
@@ -4,8 +4,8 @@ namespace Schedule.Contracts.Dtos;
 
 public class ParticipantUpdateRequest
 {
-	[EmailAddress] public string? Email { get; set; }
-	[StringLength(40)] public string? FirstName { get; set; }
-	[StringLength(40)] public string? LastName { get; set; }
-	[Phone] public string? Phone { get; set; }
+	[Required] [EmailAddress] public string Email { get; set; }
+	[Required] [StringLength(40)] public string FirstName { get; set; }
+	[Required] [StringLength(40)] public string LastName { get; set; }
+	[Required] [Phone] public string Phone { get; set; }
 }

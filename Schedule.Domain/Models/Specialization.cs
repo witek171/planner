a9675c62aeffb7fdd74a667a56260946ce85b5cb
Witold <witold778899@gmail.com>
2025-08-06 18:04:52 +0200

    add `Specialization` domain model

---
+++ Schedule.Domain/Models/Specialization.cs
@@ -0,0 +1,21 @@
+﻿namespace Schedule.Domain.Models;
+
+public class Specialization
+{
+	public Specialization(
+		Guid id,
+		Guid companyId,
+		string name,
+		string description)
+	{
+		Id = id;
+		CompanyId = companyId;
+		Name = name;
+		Description = description;
+	}
+
+	public Guid Id { get; }
+	public Guid CompanyId { get; }
+	public string Name { get; }
+	public string Description { get; }
+}

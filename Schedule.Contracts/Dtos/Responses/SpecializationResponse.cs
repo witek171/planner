f503ea2de3efd50b0c64348b5eefca8e311d2c89
Witold <witold778899@gmail.com>
2025-08-06 18:03:49 +0200

    add `SpecializationResponse` DTO

---
+++ Schedule.Contracts/Dtos/Responses/SpecializationResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schedule.Contracts.Dtos.Responses;
+
+public class SpecializationResponse
+{
+	public SpecializationResponse(
+		string name,
+		string description)
+	{
+		Name = name;
+		Description = description;
+	}
+
+	[Required] public string Name { get; }
+	[Required] public string Description { get; }
+}

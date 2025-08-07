d2dc716a40e6274e5584484fdc5128d5393a99ce
Witold <witold778899@gmail.com>
2025-08-07 15:32:17 +0200

    make `CompanyId` in `StaffMemberSpecialization` private, add `SetCompanyId` method with validation

--- Schedule.Domain/Models/StaffMemberSpecialization.cs
+++ Schedule.Domain/Models/StaffMemberSpecialization.cs
@@ -3,7 +3,7 @@
 public class StaffMemberSpecialization
 {
 	public Guid Id { get; }
-	public Guid CompanyId { get; }
+	public Guid CompanyId { get; private set; }
 	public Guid StaffMemberId { get; }
 	public Guid SpecializationId { get; }
 
@@ -17,5 +17,14 @@ public class StaffMemberSpecialization
 		CompanyId = companyId;
 		StaffMemberId = staffMemberId;
 		SpecializationId = specializationId;
+	}
+	
+	public void SetCompanyId(Guid companyId)
+	{
+		if (CompanyId != Guid.Empty)
+			throw new InvalidOperationException(
+				$"CompanyId is already set to {CompanyId} and cannot be changed");
+
+		CompanyId = companyId;
 	}
 }

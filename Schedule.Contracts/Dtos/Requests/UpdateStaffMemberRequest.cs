e31e1bf3fc76e4a3aa38c93bf49e701b34f5115e
Witold <witold778899@gmail.com>
2025-08-11 17:36:12 +0200

    add `Role` property

--- Schedule.Contracts/Dtos/Requests/UpdateStaffMemberRequest.cs
+++ Schedule.Contracts/Dtos/Requests/UpdateStaffMemberRequest.cs
@@ -1,21 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Schedule.Domain.Models.Enums;
 
 namespace Schedule.Contracts.Dtos.Requests;
 
 public class UpdateStaffMemberRequest
 {
 	public UpdateStaffMemberRequest(
+		StaffRole role,
 		string email,
 		string firstName,
 		string lastName,
 		string phone)
 	{
+		Role = role;
 		Email = email;
 		FirstName = firstName;
 		LastName = lastName;
 		Phone = phone;
 	}
 
+	[Required] public StaffRole Role { get; }
 	[Required] [EmailAddress] public string Email { get; }
 	[Required] [MaxLength(40)] public string FirstName { get; }
 	[Required] [MaxLength(40)] public string LastName { get; }

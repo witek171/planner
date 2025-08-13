4049aee405e82551636e652455af1aa56236fa1a
Witold <witold778899@gmail.com>
2025-08-13 17:34:59 +0200

    add `StaffMember` property to `StaffMemberAvailabilityResponse` and handle serialization

--- Schedule.Contracts/Dtos/Responses/StaffMemberAvailabilityResponse.cs
+++ Schedule.Contracts/Dtos/Responses/StaffMemberAvailabilityResponse.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Schedule.Contracts.Dtos.Responses;
 
@@ -7,14 +8,22 @@ public class StaffMemberAvailabilityResponse
 	public StaffMemberAvailabilityResponse(
 		DateOnly date,
 		DateTime startTime,
-		DateTime endTime)
+		DateTime endTime, StaffMemberResponse staffMember)
 	{
 		Date = date;
 		StartTime = startTime;
 		EndTime = endTime;
+		StaffMember = staffMember;
+	}
+
+	public StaffMemberAvailabilityResponse()
+	{
 	}
 
 	[Required] public DateOnly Date { get; }
 	[Required] public DateTime StartTime { get; }
 	[Required] public DateTime EndTime { get; }
+
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public StaffMemberResponse? StaffMember { get; }
 }

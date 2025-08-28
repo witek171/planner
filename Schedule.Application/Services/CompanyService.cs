664227a3211efb75ad2408208109c23f5e391ce3
Witold Pacholik <witoldpacholik@wp.pl>
2025-08-28 17:32:55 +0000

    Merged in company-controller (pull request #19)
    Company controller
    
    * refactor
    
    * simplify `DeleteByIdAsync`
    
    * add id field
    
    * add relationship management functionality
    
    * refactor
    
    * refactor remove relation logic and get relations
    
    * Merge remote-tracking branch 'origin/dev' into company-controller
    
    * extend `DeleteByIdAsync` to remove self-references in `CompanyHierarchies`
    
    * add `RelationExistAsync` method
    
    * add `HasRelationAsChildAsync`
    
    * validate parent-child relationships
    
    * rename methods and improve validation
    
    * unify `CreateCompanyRequest` and `UpdateCompanyRequest` into `CompanyRequest`, adjust formatting
    
    * add null check in `DeleteById`
    
    * fix `UnmarkCompanyAsParentIfNeededAsync` to ensure `IsParentNode` validation
    
    * improve exception message
    
    * rename route and switch `parentCompanyId` to body parameter
    
    * convert DeleteByIdAsync to expression-bodied lambda
    
    * rename `_repository` to `_companyRepository`
    
    * make `ExistsAsParentAsync` transactional, refactor and extract helper methods
    
    * convert `GetByIdAsync` to expression-bodied lambda
    
    * Merge remote-tracking branch 'origin/dev' into company-controller
    
    * add validation to prevent a company from being its own parent
    
    * relocate self-parenting validation to improve code readability and execution order
    
    * improve exception message
    
    
    Approved-by: Marcin D

---
+++ Schedule.Application/Services/CompanyService.cs
@@ -0,0 +1,104 @@
+﻿using Schedule.Application.Interfaces.Repositories;
+using Schedule.Application.Interfaces.Services;
+using Schedule.Domain.Models;
+
+namespace Schedule.Application.Services;
+
+public class CompanyService : ICompanyService
+{
+	private readonly ICompanyRepository _companyRepository;
+
+	public CompanyService(ICompanyRepository companyRepository)
+	{
+		_companyRepository = companyRepository;
+	}
+
+	public async Task<Guid> CreateAsync(Company company)
+	{
+		company.Normalize();
+		return await _companyRepository.CreateAsync(company);
+	}
+
+	public async Task PutAsync(Company company)
+	{
+		company.Normalize();
+		await _companyRepository.PutAsync(company);
+	}
+
+	public async Task DeleteByIdAsync(Guid companyId) => await _companyRepository
+		.DeleteByIdAsync(companyId);
+
+	public async Task<Company?> GetByIdAsync(Guid companyId) => await _companyRepository
+		.GetByIdAsync(companyId);
+
+	public async Task MarkAsReceptionAsync(Company company)
+	{
+		company.MarkAsReception();
+		await _companyRepository.UpdateIsReceptionFlagAsync(company);
+	}
+
+	public async Task UnmarkAsReceptionAsync(Company company)
+	{
+		company.UnmarkAsReception();
+		await _companyRepository.UpdateIsReceptionFlagAsync(company);
+	}
+
+	public async Task AddRelationAsync(
+		Guid childId,
+		Guid parentId)
+	{
+		if (childId == parentId)
+			throw new InvalidOperationException(
+				$"Company {childId} cannot be its own parent");
+
+		if (await _companyRepository.ExistsAsChildAsync(childId))
+			throw new InvalidOperationException(
+				$"Company {childId} already has a parent company");
+
+		if (await _companyRepository.RelationExistAsync(childId, parentId))
+			throw new InvalidOperationException(
+				$"Relation between companies {childId} and {parentId} already exists");
+
+		await _companyRepository.AddRelationAsync(childId, parentId);
+		Company parent = (await _companyRepository.GetByIdAsync(parentId))!;
+
+		if (!parent.IsParentNode)
+		{
+			parent.MarkAsParentNode();
+			await _companyRepository.UpdateIsParentNodeFlagAsync(parent);
+		}
+	}
+
+	public async Task RemoveRelationsAsync(Guid companyId)
+	{
+		if (!await _companyRepository.ExistsAsChildAsync(companyId) &&
+			!await _companyRepository.ExistsAsParentAsync(companyId))
+			throw new InvalidOperationException(
+				$"Company {companyId} is not present in the hierarchy," +
+				$" therefore it has no relations to remove");
+
+		(bool hasChildren, Guid? parentId) = await _companyRepository
+			.RemoveRelationsAsync(companyId);
+
+		if (!hasChildren && parentId is Guid id)
+			await UnmarkCompanyAsParentIfNeededAsync(id);
+
+		await UnmarkCompanyAsParentIfNeededAsync(companyId);
+	}
+
+	public async Task<List<Company>> GetAllRelationsAsync(Guid companyId)
+	{
+		List<Company> companies = await _companyRepository.GetAllRelationsAsync(companyId);
+		return companies;
+	}
+
+	private async Task UnmarkCompanyAsParentIfNeededAsync(Guid companyId)
+	{
+		Company company = (await _companyRepository.GetByIdAsync(companyId))!;
+		if (!await _companyRepository.ExistsAsParentAsync(companyId) && company.IsParentNode)
+		{
+			company.UnmarkAsParentNode();
+			await _companyRepository.UpdateIsParentNodeFlagAsync(company);
+		}
+	}
+}
---
+++ Schedule.Contracts/Dtos/Requests/CompanyRequest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schedule.Contracts.Dtos.Requests;
+
+public class CompanyRequest
+{
+	[Required] public string Name { get; }
+	[Required] public string TaxCode { get; }
+	[Required] public string Street { get; }
+	[Required] public string City { get; }
+	[Required] public string PostalCode { get; }
+	[Required] [Phone] public string Phone { get; }
+	[Required] [EmailAddress] public string Email { get; }
+
+	public CompanyRequest(
+		string name,
+		string taxCode,
+		string street,
+		string city,
+		string postalCode,
+		string phone,
+		string email)
+	{
+		Name = name;
+		TaxCode = taxCode;
+		Street = street;
+		City = city;
+		PostalCode = postalCode;
+		Phone = phone;
+		Email = email;
+	}
+}

7a8c702573445d4f74a2f973a42cd05790eec0d9
Witold <witold778899@gmail.com>
2025-09-02 20:42:37 +0200

    Update formatting in DTOs to improve consistency and readability

--- Schedule.Contracts/Dtos/Requests/ParticipantUpdateRequest.cs
+++ Schedule.Contracts/Dtos/Requests/ParticipantUpdateRequest.cs
@@ -4,8 +4,20 @@ namespace Schedule.Contracts.Dtos.Requests;
 
 public class ParticipantUpdateRequest
 {
-	[Required][EmailAddress] public string Email { get; }
-	[Required][StringLength(40)] public string FirstName { get; }
-	[Required][StringLength(40)] public string LastName { get; }
-	[Required][Phone] public string Phone { get; }
+	public ParticipantUpdateRequest(
+		string email,
+		string firstName,
+		string lastName,
+		string phone)
+	{
+		Email = email;
+		FirstName = firstName;
+		LastName = lastName;
+		Phone = phone;
+	}
+
+	[Required] [EmailAddress] public string Email { get; }
+	[Required] [StringLength(40)] public string FirstName { get; }
+	[Required] [StringLength(40)] public string LastName { get; }
+	[Required] [Phone] public string Phone { get; }
 }

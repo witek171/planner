d210228f5cb1265af653d3e796bb6073e0f494c4
Witold <witold778899@gmail.com>
2025-09-17 00:25:07 +0200

    Add `CompanyConfigResponse` DTO

---
+++ Schedule.Contracts/Dtos/Responses/CompanyConfigResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Schedule.Contracts.Dtos.Responses;
+
+public class CompanyConfigResponse
+{
+	[Required] public int BreakTimeStaff { get; private set; }
+	[Required] public int BreakTimeParticipants { get; private set; }
+}

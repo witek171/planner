25b52978953b488358f31939d64ca71261524d92
Witold <witold778899@gmail.com>
2025-09-17 00:24:33 +0200

    Add `CompanyConfigService`

---
+++ Schedule.Application/Services/CompanyConfigService.cs
@@ -0,0 +1,31 @@
+﻿using Schedule.Application.Interfaces.Repositories;
+using Schedule.Application.Interfaces.Services;
+using Schedule.Domain.Models;
+
+namespace Schedule.Application.Services;
+
+public class CompanyConfigService : ICompanyConfigService
+{
+	private readonly ICompanyConfigRepository _companyConfigRepository;
+
+	public CompanyConfigService(ICompanyConfigRepository companyConfigRepository)
+	{
+		_companyConfigRepository = companyConfigRepository;
+	}
+
+	public async Task UpdateBreakTimesAsync(CompanyConfig companyConfig)
+	{
+		if (companyConfig.BreakTimeStaff < 0)
+			throw new InvalidOperationException(
+				"Break time for staff must be equal or greater than zero");
+
+		if (companyConfig.BreakTimeParticipants < 0)
+			throw new InvalidOperationException(
+				"Break time for participants must be equal or greater than zero");
+
+		await _companyConfigRepository.UpdateBreakTimesAsync(companyConfig);
+	}
+
+	public async Task<CompanyConfig> GetByIdAsync(Guid companyId)
+		=> (await _companyConfigRepository.GetByIdAsync(companyId))!;
+}

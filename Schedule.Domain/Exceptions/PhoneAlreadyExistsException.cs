dd56809b9028ab66cc1c834148f5c992f8a012c2
Witold <witold778899@gmail.com>
2025-09-21 13:28:42 +0200

    Add custom exceptions

---
+++ Schedule.Domain/Exceptions/PhoneAlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace Schedule.Domain.Exceptions;
+
+public class PhoneAlreadyExistsException : Exception
+{
+	public PhoneAlreadyExistsException(string phone, Guid companyId)
+		: base($"Phone {phone} already exists for company {companyId}")
+	{
+	}
+}

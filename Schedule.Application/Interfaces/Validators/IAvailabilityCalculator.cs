0d99f008a4aec114a8f8d931cd6330fcae973d64
Witold <witold778899@gmail.com>
2025-09-29 22:06:59 +0200

    Add `AvailabilityCalculator` with interface to calculate staff availability considering active events and break times

---
+++ Schedule.Application/Interfaces/Validators/IAvailabilityCalculator.cs
@@ -0,0 +1,11 @@
+﻿using Schedule.Domain.Models;
+
+namespace Schedule.Application.Interfaces.Validators;
+
+public interface IAvailabilityCalculator
+{
+	Task<List<StaffMemberAvailability>> CalculateAvailableTimeSlots(
+		List<StaffMemberAvailability> staffMemberAvailabilities,
+		List<EventSchedule> staffMemberEvents,
+		Guid companyId);
+}

e0a0065226670ad14e5b9460d437664ed34ff5da
Witold Pacholik <witoldpacholik@wp.pl>
2025-09-29 08:57:22 +0000

    Merged in logging (pull request #30)
    Logging
    
    * Add Serilog.AspNetCore package for enhanced logging
    
    * Add Microsoft.AspNetCore.Http package to project dependencies
    
    * Add GlobalExceptionMiddleware for centralized error handling
    
    * Integrate Serilog for application logging and update middleware pipeline with GlobalExceptionMiddleware
    
    * remove try-catch
    
    * Add Microsoft.Extensions.Hosting.Abstractions package to project dependencies
    
    * Enhance GlobalExceptionMiddleware to include environment-based detailed error responses
    
    * Refactor GlobalExceptionMiddleware to simplify exception handling logic and update status code mapping
    
    * Enhance GlobalExceptionMiddleware to include detailed nested exception information
    
    * Add custom exceptions
    
    * Replace `ArgumentException` with custom exceptions
    
    * Map `EmailAlreadyExistsException` and `PhoneAlreadyExistsException` to Bad Request status
    
    * Remove Serilog logging configuration from `Program.cs`
    
    * Remove Serilog.AspNetCore package from project dependencies
    
    * Remove commented-out code from GlobalExceptionMiddleware
    
    
    Approved-by: Marcin D

--- Schedule.API/Program.cs
+++ Schedule.API/Program.cs
@@ -3,6 +3,7 @@ using Schedule.Application.Interfaces.Repositories;
 using Schedule.Application.Interfaces.Services;
 using Schedule.Application.Interfaces.Utils;
 using Schedule.Application.Services;
+using Schedule.Infrastructure.Extensions;
 using Schedule.Infrastructure.Repositories;
 using Schedule.Infrastructure.Services;
 using Schedule.Infrastructure.Utils;
@@ -72,10 +73,12 @@ public class Program
 		// Configure the HTTP request pipeline.
 		if (app.Environment.IsDevelopment())
 		{
-			app.UseDeveloperExceptionPage();
+			app.UseMiddleware<GlobalExceptionMiddleware>();
 			app.UseSwagger();
 			app.UseSwaggerUI();
 		}
+		else
+			app.UseMiddleware<GlobalExceptionMiddleware>();
 
 		app.UseHttpsRedirection();
 
